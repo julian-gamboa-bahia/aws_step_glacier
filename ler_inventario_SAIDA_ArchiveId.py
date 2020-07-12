d8649d8769980fc735ec45fa032ae369b4250c2a
julian-gamboa-bahia <mais.seguranca.android@gmail.com>
2020-07-12 17:13:03 -0300

    arquivo para gerar uma lista de ArchiveID

---
+++ ler_inventario_SAIDA_ArchiveId.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+
+import json
+
+import sys
+
+fonte=sys.argv[1]
+
+print fonte
+
+
+with open(fonte) as f:
+	data = json.load(f)
+
+for ArchiveId in data['ArchiveList']:
+	print ArchiveId['ArchiveId']
+
